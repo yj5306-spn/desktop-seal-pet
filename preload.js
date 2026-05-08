44ca2c1919ce14c33bbda8a3fb8fc7bfee543ac5
SangHyun <yj5306@sampyo.co.kr>
2026-05-08 15:22:11 +0900

    Restore native drag for performance, remove click interaction

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  dragWindow: (data) => ipcRenderer.send('drag-window', data),
   showContextMenu: () => ipcRenderer.send('show-context-menu'),
 });
